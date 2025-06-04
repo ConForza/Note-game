7eaf361b5da2641eb3a4d25cc4730cceef75591e
System Administrator <root@Garys-MacBook-Pro.local>
2025-06-04 16:29:55 +0100

    Fix service worker registration

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "vite-plugin-commonjs";
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    commonjs(),
-    tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-    }),
-  ],
+  plugins: [react(), commonjs(), tailwindcss()],
   base: "/Note-game/",
 });
