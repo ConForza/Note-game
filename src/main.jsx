93b02728fe5b6db4783c72c015c8cc56a8637e44
System Administrator <root@Garys-MacBook-Pro.local>
2025-06-05 14:52:30 +0100

    Add vite-plugin-pwa for easy caching

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")).render(<App />);
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
-      })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error);
-      });
-  });
-}
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,49 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import commonjs from "vite-plugin-commonjs";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs(), tailwindcss()],
+  plugins: [
+    react(),
+    commonjs(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["**/*"],
+      manifest: {
+        name: "Note Game",
+        short_name: "NoteGame",
+        description: "A note game to assist with musical sight-reading",
+        theme_color: "#ffedd5",
+        background_color: "#ffedd5",
+        display: "standalone",
+        icons: [
+          {
+            src: "note-game.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "CacheFirst",
+            options: {
+              cacheName: "images-cache",
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
+              },
+            },
+          },
+        ],
+        globPatterns: ["**/*"],
+      },
+    }),
+  ],
   base: "/Note-game/",
 });
