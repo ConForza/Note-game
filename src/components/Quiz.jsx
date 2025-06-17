ec613ac40aa90488000626d0846cfc2e1bbfb38d
System Administrator <root@Garys-MacBook-Pro.local>
2025-06-17 13:38:19 +0100

    Change landscape appearance

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -19,24 +19,26 @@ export default function Quiz({
           buttonState={buttonState}
         />
       </div>
-      <div className="flex flex-col gap-6">
+      <div className="question-div flex flex-col gap-6">
         <div className="flex flex-row justify-center h-50 min-w-full bg-white rounded-lg">
           <img
             className="self-center"
             src={`./${chosenNote.type}-${chosenNote.id}.png`}
           ></img>
         </div>
-        <ul className="questions flex flex-col gap-2">
-          {answerOptions.map((answer, index) => (
-            <Notename
-              key={answer}
-              id={index}
-              answer={answer}
-              handleAnswers={handleAnswers}
-              buttonState={buttonState}
-            />
-          ))}
-        </ul>
+        <div>
+          <ul className="questions flex flex-col gap-2">
+            {answerOptions.map((answer, index) => (
+              <Notename
+                key={answer}
+                id={index}
+                answer={answer}
+                handleAnswers={handleAnswers}
+                buttonState={buttonState}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
