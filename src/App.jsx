77d38119eec5f6d4b8db2496021fb656eb3ab5fa
Gary O'Shea <gary@garyoshea.co.uk>
2026-05-05 09:25:29 +0100

    Fix pwa orientation bug

--- src/App.jsx
+++ src/App.jsx
@@ -62,22 +62,24 @@ function App() {
 
   return (
     <div className={noteSettings.isDarkMode ? "dark" : ""}>
-      <div className="min-h-svh w-full bg-orange-100 dark:bg-neutral-900 text-gray-900 dark:text-neutral-100 flex flex-col items-center justify-center">
-        <div className="inner-container min-w-90 p-6 flex justify-center">
-          {route === "quiz" && (
-            <QuizPage
-              noteSettings={noteSettings}
-              handleRestartQuiz={() => handleRoute("menu")}
-              handleHighScore={handleHighScore}
-              highScore={highScore}
-            />
-          )}
-          {route === "menu" && (
-            <>
+      <div className="min-h-svh w-full bg-orange-100 dark:bg-neutral-900 text-gray-900 dark:text-neutral-100 flex flex-col">
+        <main className="flex-1 flex items-center justify-center">
+          <div className="inner-container min-w-90 p-6 flex justify-center">
+            {route === "quiz" && (
+              <QuizPage
+                noteSettings={noteSettings}
+                handleRestartQuiz={() => handleRoute("menu")}
+                handleHighScore={handleHighScore}
+                highScore={highScore}
+              />
+            )}
+
+            {route === "menu" && (
               <div className="text-center">
                 <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold leading-tight tracking-tight bg-gradient-to-r from-blue-500 to-blue-300 text-transparent bg-clip-text mb-8">
                   Note Game
                 </h1>
+
                 <div className="space-x-4 mt-6 mb-10">
                   <button
                     onClick={() => handleRoute("quiz")}
@@ -85,6 +87,7 @@ function App() {
                   >
                     Start
                   </button>
+
                   <button
                     onClick={() => handleRoute("settings")}
                     className="bg-gray-300 text-gray-800 w-30 py-3 rounded-lg shadow-lg dark:bg-neutral-700 dark:text-gray-100 dark:hover:bg-neutral-600 hover:bg-gray-400 transition"
@@ -93,19 +96,23 @@ function App() {
                   </button>
                 </div>
               </div>
-              <footer>Gary O'Shea, 2025</footer>
-            </>
-          )}
-          {route === "settings" && (
-            <Settings
-              handleSettingsChange={handleSettingsChange}
-              handleRestartQuiz={() => handleRoute("menu")}
-              handleDarkMode={handleDarkMode}
-              settings={noteSettings}
-              handleHighScore={handleHighScore}
-            />
-          )}
-        </div>
+            )}
+
+            {route === "settings" && (
+              <Settings
+                handleSettingsChange={handleSettingsChange}
+                handleRestartQuiz={() => handleRoute("menu")}
+                handleDarkMode={handleDarkMode}
+                settings={noteSettings}
+                handleHighScore={handleHighScore}
+              />
+            )}
+          </div>
+        </main>
+
+        {route === "menu" && (
+          <footer className="text-center p-5">Gary O'Shea, 2025</footer>
+        )}
       </div>
     </div>
   );
